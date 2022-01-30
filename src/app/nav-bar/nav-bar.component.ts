80eb958b0a8d87308ec1ac0deadfbf4e6371b4ad
gaviroth <89350316+gaviroth@users.noreply.github.com>
2022-01-30 15:00:57 +0200

    img added

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -12,13 +12,17 @@ export class NavBarComponent implements OnInit {
   rightCaption: string = "right";
   inIncome: number;
   inKids: number;
+  imgstr: string = "none";
   inType : string = ""
   ans1 : string = ""
   ans2 : string = ""
   ans3 : string = ""
   ans4 : string = ""
+  ans5 : string = ""
+  img1 : string = ""
   num: number = 1;
   tax: number;
+  
 
   currentlySelected: string = "middle";
 
@@ -33,7 +37,7 @@ export class NavBarComponent implements OnInit {
   }
 
   logval(income: number,kids: number,type: string){
-
+    this.img1 = "";
     this.inKids = kids;
     this.inIncome = income;
     this.inType = type;
@@ -42,6 +46,15 @@ export class NavBarComponent implements OnInit {
     this.ans2 =  "₪" + income*3;
     this.ans3 = "מיסים למדינה";
     this.ans4 = "יש לך " + kids + "ילדים ואתה גר ב" + type;
+    this.ans5 = "המחשה של איפה המיסים שלך עוזרים:" ;
+    if(income*3 > 100){
+      this.imgstr = "block"
+      this.img1 = "/assets/img/abc.png";
+    }
+    else{
+      this.imgstr = "block"
+      this.img1 = "/assets/img/bca.png";
+    }
     
   }
 }
