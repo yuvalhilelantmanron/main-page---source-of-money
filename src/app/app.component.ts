aec37f13c5cff687bd00aeea9f18a0ece6037cf7
chillyproductions <54706474+chillyproductions@users.noreply.github.com>
2022-04-01 18:21:08 +0300

    bubble sub categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,8 @@ export class AppComponent implements OnInit {
   public data: any;
 
   constructor(@Inject(BUBBLES) private bubbles: any) {
-    this.getData(bubbles);
+    // this.getData(bubbles);
+    this.newGetData();
   }
 
   async getData(bubbles) {
@@ -65,6 +66,48 @@ export class AppComponent implements OnInit {
 
     minimizedData.sort((a, b) => b.amount - a.amount)
     this.funcCategories = minimizedData;
+  }
+
+    async newGetData() {
+    //fetches data from url
+    var raw = await fetch('https://next.obudget.org/api/query?query=SELECT%20year,%20func_cls_title_2-%3E%3E0%20as%20func_title,%20title,%20sum(net_revised)%20AS%20revised,%20sum(net_allocated)%20AS%20allocated,%20sum(net_executed)%20AS%20executed%20FROM%20budget%20WHERE%20depth%20=%202%20and%20code%20like%20%270000%%%27%20group%20by%201,%202,%203%20order%20by%202,%201');
+    var data = await raw.json();
+    data = data.rows;
+    this.data = data;
+
+    //gets the most current year
+    var lastTitle = data[0].func_title;
+    for (var item of data) {
+      if (item.func_title != lastTitle) break;
+      this.year = Math.max(this.year, item.year);
+    }
+
+    //creates an ordered diconary from data
+    var minimizedData = {};
+    this.totalAmount = 0;
+    for (var item of data) {
+      if (item.year != this.year || item.func_title == "הכנסות למימון גירעון" || item.allocated == 0) continue;
+
+      if(!minimizedData[item.func_title])
+        minimizedData[item.func_title] = {name: item.func_title, scale: 1, amount: 0, values:{}};
+      
+      minimizedData[item.func_title].amount += item.allocated;
+      minimizedData[item.func_title].values[item.title] = {amount:item.allocated};
+      this.totalAmount += item.allocated;
+    }
+
+    //turns diconary into array
+    var minimizedArray = [];
+    for(let item in minimizedData){
+      minimizedArray.push(minimizedData[item]);      
+    }
+
+    //gets the percentage of each category
+    for (item of minimizedArray)
+      item.percent = (item.amount / this.totalAmount) * 100;
+
+    minimizedArray.sort((a, b) => b.amount - a.amount)
+    this.funcCategories = minimizedArray;
   }
 
   @HostListener('window:scroll', ['$event'])
