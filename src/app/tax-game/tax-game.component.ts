616fe8dd14ccdde2f4855af9db6086cbdbe08567
Ethan Levy <ethan@ethanlevy.org>
2022-04-02 22:02:51 +0300

    Update tax selction
    - Change radio buttons to tab selection
    - Add basic selection functionality

--- src/app/tax-game/tax-game.component.ts
+++ src/app/tax-game/tax-game.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tax-game.component.less']
 })
 export class TaxGameComponent implements OnInit {
+  tax1Cng = "+1";
+  tax2Cng = "+2";
+  tax3Cng = "+3";
+  tax4Cng = "+4";
+  tax5Cng = "+5";
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public changeTax(evt, tax) {
+    // TODO: Change tax type
+
+    // Remove highlight
+    let taxButtons = document.getElementsByClassName("tax-btn");
+    for (let i = 0; i < taxButtons.length; i++) {
+      taxButtons[i].className = taxButtons[i].className.replace(" active", "");
+    }
+
+    // Add highlight to selected button
+    evt.currentTarget.className += " active";
+  }
 }
