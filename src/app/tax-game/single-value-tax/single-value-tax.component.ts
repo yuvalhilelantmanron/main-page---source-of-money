b6f9143f76b30b70399e21e5254197718ecb3b2e
alona-daphna <alona.daphna@gmail.com>
2022-04-07 14:39:58 +0300

    fetch current value from api

--- src/app/tax-game/single-value-tax/single-value-tax.component.ts
+++ src/app/tax-game/single-value-tax/single-value-tax.component.ts
@@ -11,17 +11,29 @@ export class SingleValueTaxComponent implements OnInit {
   new_rate: number;
   taxes: any = {
     "delek": {
-      current_value: 21090000000, 
+      get_current_value: async () => {
+        var raw = await fetch("https://next.obudget.org/api/query?query=SELECT%20code,year,net_executed,history%20FROM%20budget%20WHERE%20code=%270000180101%27");
+        var data = await raw.json();
+        return data.rows[2].history["2020"].net_executed
+      },
       current_rate: 30,
       text_to_show: "אגורות לליטר"
     },
-    "income": {
-      current_value: 167297000000,
-      current_rate: 25,
+    "maam": {
+      get_current_value: async () => {
+        var raw = await fetch("https://next.obudget.org/api/query?query=SELECT%20code,year,net_executed,history,title%20FROM%20budget%20WHERE%20code=%270000140201%27");
+        var data = await raw.json();
+        return data.rows[1].history["2020"].net_executed
+      },
+      current_rate: 17,
       text_to_show: "אחוזים"
     },
     "tabak": {
-      current_value: 600000000,
+      get_current_value: async () => {
+        var raw = await fetch("https:next.obudget.org/api/query?query=SELECT%20code,year,net_executed,history%20FROM%20budget%20WHERE%20code=%2700001501%27AND%20year=%272020%27");
+        var data = await raw.json();
+        return data.rows[0].net_executed
+      },
       current_rate: 459,
       text_to_show: 'ש"ח לק"ג'
     }
@@ -30,16 +42,17 @@ export class SingleValueTaxComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
   getTax(){
     return this.tax_type;
   }
 
-  getDiff() {
-    var new_value = this.taxes[this.tax_type].current_value / (this.taxes[this.tax_type].current_rate/100) * (this.new_rate/100);
+  async getDiff() {
+    var new_value = await this.taxes[this.tax_type].get_current_value() / (this.taxes[this.tax_type].current_rate/100) * (this.new_rate/100);
 
-    this.onChange.emit({value: new_value - this.taxes[this.tax_type].current_value})
+    this.onChange.emit({value: new_value - await this.taxes[this.tax_type].get_current_value()})
   }
 
 }
