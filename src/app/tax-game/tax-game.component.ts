84142a4110771e3960dd9d32f455a1fa6fbab9b6
Ethan Levy <ethan@ethanlevy.org>
2022-04-08 16:11:35 +0300

    Link total/new income to diff

--- src/app/tax-game/tax-game.component.ts
+++ src/app/tax-game/tax-game.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tax-game.component.less']
 })
 export class TaxGameComponent implements OnInit {
+  totalIncome: number = 123456789; // TODO: Get real total income
+
   diff: number = 0;
 
   tax1Cng = "+1";
@@ -37,5 +39,18 @@ export class TaxGameComponent implements OnInit {
 
     // Add highlight to selected button
     evt.currentTarget.className += " active";
+  }
+
+
+  formatNum(num) {
+    // Adds commas to long numbers (123456 -> 123,456)
+    var str = num.toString().split('.');
+    if (str[0].length >= 5) {
+        str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+    if (str[1] && str[1].length >= 5) {
+        str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+    }
+    return str.join('.');
   }
 }
