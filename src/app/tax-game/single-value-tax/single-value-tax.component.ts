d275ce2873ef26220d6a294d2037c0aa0bf02646
alona-daphna <74145815+alona-daphna@users.noreply.github.com>
2022-04-10 23:35:26 +0300

    fetch current value from api (#8)
    Modify single-value-tax component to fetch value from online DB

--- src/app/tax-game/single-value-tax/single-value-tax.component.ts
+++ src/app/tax-game/single-value-tax/single-value-tax.component.ts
@@ -9,19 +9,35 @@ export class SingleValueTaxComponent implements OnInit {
   @Input() tax_type: any;
   @Output() onChange = new EventEmitter();
   new_rate: number;
+  latest_year: number = 2020;
   taxes: any = {
     "delek": {
-      current_value: 21090000000, 
-      current_rate: 30,
-      text_to_show: "אגורות לליטר"
+      get_current_value: async () => {
+        var tax_code = '0000180101';
+        var raw = await fetch("https://next.obudget.org/api/query?query=SELECT%20history%20FROM%20budget%20WHERE%20code=%27"+tax_code+"%27AND%20year>=2020%20ORDER%20BY%20year%20ASC%20LIMIT%201");
+        var data = await raw.json();
+        return data.rows[0].history[this.latest_year].net_executed;
+      },
+      current_rate: 3.13,
+      text_to_show: "שקלים לליטר"
     },
-    "income": {
-      current_value: 167297000000,
-      current_rate: 25,
+    "maam": {
+      get_current_value: async () => {
+        var tax_code = '0000140201';
+        var raw = await fetch("https://next.obudget.org/api/query?query=SELECT%20history%20FROM%20budget%20WHERE%20code=%27"+tax_code+"%27AND%20year>=2020%20ORDER%20BY%20year%20ASC%20LIMIT%201");
+        var data = await raw.json();
+        return data.rows[0].history[this.latest_year].net_executed;
+      },
+      current_rate: 17,
       text_to_show: "אחוזים"
     },
     "tabak": {
-      current_value: 600000000,
+      get_current_value: async () => {
+        var tax_code = '00001501';
+        var raw = await fetch("https://next.obudget.org/api/query?query=SELECT%20net_executed%20FROM%20budget%20WHERE%20code=%27"+tax_code+"%27AND%20year=%27"+this.latest_year+"%27");
+        var data = await raw.json();
+        return data.rows[0].net_executed
+      },
       current_rate: 459,
       text_to_show: 'ש"ח לק"ג'
     }
@@ -29,17 +45,17 @@ export class SingleValueTaxComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
   }
 
   getTax(){
     return this.tax_type;
   }
 
-  getDiff() {
-    var new_value = this.taxes[this.tax_type].current_value / (this.taxes[this.tax_type].current_rate/100) * (this.new_rate/100);
+  async getDiff() {
+    var new_value = await this.taxes[this.tax_type].get_current_value() / this.taxes[this.tax_type].current_rate * this.new_rate;
 
-    this.onChange.emit({value: new_value - this.taxes[this.tax_type].current_value})
+    this.onChange.emit({value: new_value - await this.taxes[this.tax_type].get_current_value()})
   }
 
 }
