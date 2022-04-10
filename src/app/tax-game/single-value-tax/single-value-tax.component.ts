2d3722e74ca19b43fdf53e249ddc1189bbbc7c96
alona-daphna <alona.daphna@gmail.com>
2022-04-10 22:18:55 +0300

    sort json for the right year

--- src/app/tax-game/single-value-tax/single-value-tax.component.ts
+++ src/app/tax-game/single-value-tax/single-value-tax.component.ts
@@ -9,28 +9,32 @@ export class SingleValueTaxComponent implements OnInit {
   @Input() tax_type: any;
   @Output() onChange = new EventEmitter();
   new_rate: number;
+  latest_year: number = 2020;
   taxes: any = {
     "delek": {
+      code: '0000180101',
       get_current_value: async () => {
-        var raw = await fetch("https://next.obudget.org/api/query?query=SELECT%20code,year,net_executed,history%20FROM%20budget%20WHERE%20code=%270000180101%27");
+        var raw = await fetch("https://next.obudget.org/api/query?query=SELECT%20history,year%20FROM%20budget%20WHERE%20code=%27"+this.taxes['delek'].code+"%27");
         var data = await raw.json();
-        return data.rows[2].history["2020"].net_executed
+        return this.getData(data);
       },
-      current_rate: 30,
-      text_to_show: "אגורות לליטר"
+      current_rate: 3.13,
+      text_to_show: "שקלים לליטר"
     },
     "maam": {
+      code: '0000140201',
       get_current_value: async () => {
-        var raw = await fetch("https://next.obudget.org/api/query?query=SELECT%20code,year,net_executed,history,title%20FROM%20budget%20WHERE%20code=%270000140201%27");
+        var raw = await fetch("https://next.obudget.org/api/query?query=SELECT%20history,year%20FROM%20budget%20WHERE%20code=%27"+this.taxes['maam'].code+"%27");
         var data = await raw.json();
-        return data.rows[1].history["2020"].net_executed
+        return this.getData(data);
       },
       current_rate: 17,
       text_to_show: "אחוזים"
     },
     "tabak": {
+      code: '00001501',
       get_current_value: async () => {
-        var raw = await fetch("https:next.obudget.org/api/query?query=SELECT%20code,year,net_executed,history%20FROM%20budget%20WHERE%20code=%2700001501%27AND%20year=%272020%27");
+        var raw = await fetch("https://next.obudget.org/api/query?query=SELECT%20net_executed%20FROM%20budget%20WHERE%20code=%27"+this.taxes['tabak'].code+"%27AND%20year=%27"+this.latest_year+"%27");
         var data = await raw.json();
         return data.rows[0].net_executed
       },
@@ -41,8 +45,17 @@ export class SingleValueTaxComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
+  }
 
+  getData(data) {
+    data = data.rows;
+    for (var key in data) {
+      if(data[key].year > this.latest_year)
+        data = data[key];
+    }
+
+    return data.history[this.latest_year].net_executed;
   }
 
   getTax(){
@@ -50,7 +63,7 @@ export class SingleValueTaxComponent implements OnInit {
   }
 
   async getDiff() {
-    var new_value = await this.taxes[this.tax_type].get_current_value() / (this.taxes[this.tax_type].current_rate/100) * (this.new_rate/100);
+    var new_value = await this.taxes[this.tax_type].get_current_value() / this.taxes[this.tax_type].current_rate * this.new_rate;
 
     this.onChange.emit({value: new_value - await this.taxes[this.tax_type].get_current_value()})
   }
