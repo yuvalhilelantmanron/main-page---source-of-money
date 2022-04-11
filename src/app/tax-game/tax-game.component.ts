dbeca962a7237cff7ae9719f30c626062aedc9f3
Ethan Levy <ethan@ethanlevy.org>
2022-04-11 10:56:47 +0300

    Change fetching method

--- src/app/tax-game/tax-game.component.ts
+++ src/app/tax-game/tax-game.component.ts
@@ -8,7 +8,7 @@ import { UtilsService } from '../utils.service'
 })
 export class TaxGameComponent implements OnInit {
   public totalIncome: number = 0;
-  public year: number = 0;
+  public year: number = 2020;
 
   diff: number = 0;
 
@@ -33,25 +33,10 @@ export class TaxGameComponent implements OnInit {
   }
 
   async getData() {
-    //fetches data from url
-    var raw = await fetch('https://next.obudget.org/api/query?query=SELECT%20year,%20func_cls_title_2-%3E%3E0%20as%20func_title,%20title,%20sum(net_revised)%20AS%20revised,%20sum(net_allocated)%20AS%20allocated,%20sum(net_executed)%20AS%20executed%20FROM%20budget%20WHERE%20depth%20=%202%20and%20code%20like%20%270000%%%27%20group%20by%201,%202,%203%20order%20by%202,%201');
+    var tax_code = '0000';
+    var raw = await fetch("https://next.obudget.org/api/query?query=SELECT%20history%20FROM%20budget%20WHERE%20code=%27"+tax_code+"%27AND%20year>=2020%20ORDER%20BY%20year%20ASC%20LIMIT%201");
     var data = await raw.json();
-    data = data.rows;
-
-    //gets the most current year
-    var lastTitle = data[0].func_title;
-    for (var item of data) {
-      if (item.func_title != lastTitle) break;
-      this.year = Math.max(this.year, item.year);
-    }
-
-    // Add income value
-    this.totalIncome = 0;
-    for (var item of data) {
-      if (item.year != this.year || item.func_title == "הכנסות למימון גירעון" || item.allocated == 0) continue;
-
-      this.totalIncome += item.allocated;
-    }
+    this.totalIncome = data.rows[0].history[this.year].net_executed;
   }
 
   public changeTax(evt, tax) {
