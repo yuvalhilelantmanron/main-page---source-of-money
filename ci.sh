2c4e79943b006f332fa183d997fbc8122a41a5c8
Inbar Aran <inbar.aran@gmail.com>
2022-04-12 13:10:18 +0300

    Make owner inbararan

--- ci.sh
+++ ci.sh
@@ -8,7 +8,7 @@ DEPLOY_GIT_USER=budgetkey-deployer
 
 DEPLOY_COMMIT_MESSAGE="automatic update of source-of-money-app-main-page"
 DEPLOY_VALUES_CHART_NAME=source-of-money-app-main-page
-DOCKER_IMAGE=ghcr.io/budgetkey/source-of-money-app-main-page
+DOCKER_IMAGE=ghcr.io/inbararan/source-of-money-app-main-page
 
 
 if [ "${1}" == "install" ]; then
