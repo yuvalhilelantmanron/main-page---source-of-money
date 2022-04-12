436d1441ad0c63a2fc833fd3db7361190c3c949e
nuti <nati@reitblat.com>
2022-04-12 14:23:52 +0300

    map component
    added a map component with a heat map of where do most of the taxes come from. A better data set is needed for it to be complete

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, Input } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import * as mapData from '../mapData.json';
 
@@ -10,9 +10,10 @@ import * as mapData from '../mapData.json';
 })
 export class MapComponent implements OnInit {
   map: mapboxgl.Map;
-  style = 'mapbox://styles/mapbox/navigation-day-v1';
+  style = 'mapbox://styles/mapbox/light-v10';
   lat = 31.55;
   lng = 34.99;
+  hoveredStateId = null;
 
   constructor() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYnVkZ2V0a2V5IiwiYSI6ImNsMWdscGN4cjAwcXUzZHVqMTNubGJvODQifQ.-3UrIwz_R4UcmNFpfSlPKg';
@@ -32,23 +33,37 @@ export class MapComponent implements OnInit {
 
     this.map.on('load', () => {
       // console.log((mapData as any).default);
-      this.map.addSource('cityData', {
+      this.map.addSource('city_data', {
         'type': 'geojson',
         'data': (mapData as any).default
       });
 
+      // this.map.addLayer(
+      //   {
+      //     id: "city_layout",
+      //     source: 'city_data',
+      //     type: 'line',
+      //     color: 'black'
+      //   }
+      // )
 
-      this.map.addLayer(
-        {
-          "id": "city",
-          "source": "cityData",
-          "type": "circle",
-          "circle-radius": {
-            property: 'population',
-            type: 'exponential',
-          }
+      this.map.addLayer({
+        'id': 'city_fill',
+        'type': 'fill',
+        'source': 'city_data',
+        'paint': {
+          'fill-color':
+            [
+              "interpolate",
+              ["linear"],
+              ["get", "@id"],
+              0,
+              "hsl(0, 7%, 11%)",
+              4,
+              "hsl(360, 100%, 54%)"
+            ]
         }
-      );
+      });
     })
   }
 }
