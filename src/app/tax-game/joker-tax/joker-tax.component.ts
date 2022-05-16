8d650b7272022456895611595731be1e5685db16
alona-daphna <alona.daphna@gmail.com>
2022-05-16 08:15:48 +0300

    fetch taxes and display the options

--- src/app/tax-game/joker-tax/joker-tax.component.ts
+++ src/app/tax-game/joker-tax/joker-tax.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchingService } from 'src/app/fetching.service';
 
 @Component({
   selector: 'app-joker-tax',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./joker-tax.component.less']
 })
 export class JokerTaxComponent implements OnInit {
+  taxes: Array<any> = [];
+  tax: any;
 
-  constructor() { }
+  constructor(private fetching: FetchingService) { }
 
   ngOnInit() {
+  }
+
+  async fetchTaxes(other_tax:string) {
+    let data = await this.fetching.fetchTaxByTitle(other_tax);
+    this.taxes = data;
+  }
+
+  onClick(chosen_tax) {
+    this.tax = chosen_tax;
   }
 
 }
