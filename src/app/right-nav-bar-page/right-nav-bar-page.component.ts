d562b94822a6fdb792049c249145f90fc4de3771
nuti <nati@reitblat.com>
2022-06-23 17:34:18 +0300

    new industry data

--- src/app/right-nav-bar-page/right-nav-bar-page.component.ts
+++ src/app/right-nav-bar-page/right-nav-bar-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as industryData from '../industryData.json';
+import { UtilsService } from '../utils.service';
 
 @Component({
   selector: 'right-nav-bar-page',
@@ -9,12 +10,17 @@ import * as industryData from '../industryData.json';
 export class RightNavBarPageComponent implements OnInit {
   industryData: any = industryData.data;
 
-  constructor() { }
+  constructor(private utils: UtilsService) { }
 
   ngOnInit() {
-    var sum = this.industryData.reduce((sum, curr) => sum + curr.amount, 0);
+    // var sum = this.industryData.reduce((sum, curr) => sum + curr.taxSum, 0);
+    var sum = 0;
+    console.log(this.industryData)
     for (let industry of this.industryData) {
-      industry.headerSize = 10 * industry.amount / sum + 1;
+      sum += industry.taxSum;
+    }
+    for (let industry of this.industryData) {
+      industry.headerSize = 10 * industry.taxSum / sum + 1;
     }
   }
 
